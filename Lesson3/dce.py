b4308f03a90a8fbf75d432c5d6ccdedfb8968268
chhzh123 <hc676@cornell.edu>
2022-02-08 10:12:58 -0500

    [L3] Add while loop for DCE

--- Lesson3/dce.py
+++ Lesson3/dce.py
@@ -14,33 +14,38 @@ def dce(prg):
     top_func = func_map["main"]
 
     def runOnFunction(func):
-        # find the used list
-        used = set()
-        for instr in func["instrs"]:
-            if "args" in instr:
-                for arg in instr["args"]:
-                    used.add(arg)
-        # traverse again to check whether those args are used
-        new_instrs = []
-        for instr in func["instrs"]:
-            if "dest" in instr and instr["dest"] not in used:
-                pass
-            else:
-                new_instrs.append(instr)
-        func["instrs"] = new_instrs.copy()
-        # reassignment
-        last_def = {} # var->instr
-        for instr in func["instrs"]:
-            # check for uses
-            if "args" in instr:
-                for arg in instr["args"]:
-                    last_def.pop(arg, None)
-            # check for defs
-            if "dest" in instr:
-                if instr["dest"] in last_def:
-                    new_instrs.remove(last_def[instr["dest"]])
-                last_def[instr["dest"]] = instr
-        func["instrs"] = new_instrs.copy()
+        while True:
+            prev_size = len(func["instrs"])
+            # find the used list
+            used = set()
+            for instr in func["instrs"]:
+                if "args" in instr:
+                    for arg in instr["args"]:
+                        used.add(arg)
+            # traverse again to check whether those args are used
+            new_instrs = []
+            for instr in func["instrs"]:
+                if "dest" in instr and instr["dest"] not in used:
+                    pass
+                else:
+                    new_instrs.append(instr)
+            func["instrs"] = new_instrs.copy()
+            # reassignment
+            last_def = {} # var->instr
+            for instr in func["instrs"]:
+                # check for uses
+                if "args" in instr:
+                    for arg in instr["args"]:
+                        last_def.pop(arg, None)
+                # check for defs
+                if "dest" in instr:
+                    if instr["dest"] in last_def:
+                        new_instrs.remove(last_def[instr["dest"]])
+                    last_def[instr["dest"]] = instr
+            func["instrs"] = new_instrs.copy()
+            curr_size = len(func["instrs"])
+            if prev_size == curr_size:
+                break
 
     for func in funcs:
         runOnFunction(func)
