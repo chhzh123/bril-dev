caec0a0c421048038a40def200b1e4628965daeb
chhzh123 <hc676@cornell.edu>
2022-02-16 22:54:42 -0500

    [L4] Re-structure

--- Lesson4/dfa.py
+++ Lesson4/dfa.py
@@ -20,61 +20,60 @@ while worklist is not empty:
         worklist += successors of b
 """
 
-def dfa(prg):
+def dfa(cfg, init, merge, transfer):
     """Dataflow Analysis
     """
-    funcs = prg["functions"]
+    preds, succs = get_edges(cfg)
 
-    # first find the top-level function
-    # and record the function map
-    func_map = {}
+    in_b = {}
+    out_b = {}
+    # find entry block
+    for b_name in preds:
+        if len(preds[b_name]) == 0:
+            entry = b_name
+            break
+
+    # initialization
+    worklist = []
+    in_b[entry] = init
+    for b_name in cfg:
+        if b_name != entry:
+            in_b[b_name] = set() # placeholder
+        out_b[b_name] = init
+        worklist.append(b_name)
+
+    # worklist algorithm
+    while len(worklist) != 0:
+        b_name = worklist.pop(0) # from front
+        # print(b_name)
+        for pred in preds[b_name]:
+            in_b[b_name] = merge(in_b[b_name], out_b[pred])
+        new_v = transfer(cfg[b_name], in_b[b_name])
+        # print("out_v", new_v)
+        if out_b[b_name] != new_v:
+            for succ in succs[b_name]:
+                if succ not in worklist:
+                    worklist.append(succ)
+            out_b[b_name] = new_v
+
+    return in_b, out_b
+
+def reaching_defition(func):
     cnt_def = 0
-    for func in funcs:
-        func_map[func["name"]] = func
-        # label instructions
-        for instr in func["instrs"]:
-            if "dest" in instr: # definition
-                instr["id"] = "d{}_{}".format(cnt_def, instr["dest"])
-                cnt_def += 1
+    # label instructions
+    for instr in func["instrs"]:
+        if "dest" in instr: # definition
+            instr["id"] = "d{}_{}".format(cnt_def, instr["dest"])
+            cnt_def += 1
 
-    def runOnFunction(cfg, init, merge, transfer):
-        preds, succs = get_edges(cfg)
-
-        in_b = {}
-        out_b = {}
-        # find entry block
-        for b_name in preds:
-            if len(preds[b_name]) == 0:
-                entry = b_name
-                break
-
-        # initialization
-        worklist = []
-        in_b[entry] = init
-        for b_name in cfg:
-            if b_name != entry:
-                in_b[b_name] = set() # placeholder
-            out_b[b_name] = init
-            worklist.append(b_name)
-
-        # worklist algorithm
-        while len(worklist) != 0:
-            b_name = worklist.pop(0) # from front
-            # print(b_name)
-            for pred in preds[b_name]:
-                in_b[b_name] = merge(in_b[b_name], out_b[pred])
-            new_v = transfer(cfg[b_name], in_b[b_name])
-            # print("out_v", new_v)
-            if out_b[b_name] != new_v:
-                for succ in succs[b_name]:
-                    if succ not in worklist:
-                        worklist.append(succ)
-                out_b[b_name] = new_v
-
-        return in_b, out_b
+    # function arguments are also definitions
+    init_def = set()
+    if "args" in func:
+        for idx, arg in enumerate(func["args"]):
+            def_id = "arg{}_{}".format(idx, arg["name"])
+            init_def.add(def_id)
 
     def transfer_reaching_def(block, in_b): # work on instructions
-        # find def_b
         def_b = set()
         kill_b = set()
         for instr in block:
@@ -84,41 +83,42 @@ def dfa(prg):
                     if instr["dest"] == def_var:
                         kill_b.add(def_id)
                 def_b.add(instr["id"])
-        # print("def",def_b,"kill",kill_b)
         return def_b.union(in_b - kill_b)
 
-    for func in funcs:
-        # construct cfg
-        name2block = block_map(form_blocks(func['instrs']))
+    in_b, out_b = dfa(name2block,
+                      init_def,
+                      lambda x, y: x.union(y),
+                      transfer_reaching_def)
+    return in_b, out_b
 
-        # Insert terminators into blocks that don't have them
-        add_terminators(name2block)
-
-        # function arguments are also definitions
-        init_def = set()
-        if "args" in func:
-            for idx, arg in enumerate(func["args"]):
-                def_id = "arg{}_{}".format(idx, arg["name"])
-                init_def.add(def_id)
-        in_b, out_b = runOnFunction(name2block,
-                                    init_def,
-                                    lambda x, y: x.union(y),
-                                    transfer_reaching_def)
-
-        return in_b, out_b
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process command line arguments')
     parser.add_argument('-f', dest='file', default="", help='get input file')
-    parser.add_argument('-reach_def', dest='reach_def', action='store_true', help='multiple runs')
+    parser.add_argument('-reach_def', dest='reach_def', action='store_true', help='reaching definition')
     args = parser.parse_args()
     if args.file != "":
         with open(args.file, "r") as infile:
             program = json.load(infile)
     else:
         program = json.loads(''.join(sys.stdin.readlines())) # already in json format
-    in_b, out_b = dfa(program)
-    for item in zip(in_b.items(), out_b.items()):
-        print("{}:".format(item[0][0]))
-        print("in", item[0][1])
-        print("out", item[1][1])
+
+    # find top level function
+    funcs = program["functions"]
+    for func in funcs:
+        if func["name"] == "main":
+            break
+    else:
+        raise RuntimeError("Do not have main function")
+
+    # construct cfg
+    name2block = block_map(form_blocks(func['instrs']))
+    # Insert terminators into blocks that don't have them
+    add_terminators(name2block)
+
+    if args.reach_def:
+        in_b, out_b = reaching_defition(func)
+        for item in zip(in_b.items(), out_b.items()):
+            print("{}:".format(item[0][0]))
+            print("  in: ", ", ".join(item[0][1]))
+            print("  out:", ", ".join(item[1][1]))
