13e7a94376db1e3eca3edc489544a5ae55b61609
chhzh123 <hc676@cornell.edu>
2022-03-15 21:47:25 -0400

    [L7] Add print for indices

--- Lesson7/skeleton/LoopAnalysis.cpp
+++ Lesson7/skeleton/LoopAnalysis.cpp
@@ -1,7 +1,4 @@
-#include "llvm/Analysis/LoopAccessAnalysis.h"
-#include "llvm/Analysis/LoopAnalysisManager.h"
 #include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Pass.h"
@@ -53,62 +50,50 @@ struct LoopAnalysisPass : public FunctionPass {
   LoopAnalysisPass() : FunctionPass(ID) {}
 
   virtual bool runOnFunction(Function &F) {
-    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
-      for (BasicBlock::iterator instr = bb->begin(), e = bb->end(); instr != e;
-           ++instr) {
-        // https://llvm.org/doxygen/classllvm_1_1Instruction.html
-        // ref: llvm/lib/Transforms/Scalar/LoopFlatten.cpp
-        if (auto *GEP = dyn_cast<GetElementPtrInst>(instr)) {
-          for (Value *GEPUser : instr->users()) {
-            auto *GEPUserInst = cast<Instruction>(GEPUser);
-            if (isa<LoadInst>(GEPUserInst) || isa<StoreInst>(GEPUserInst)) {
-              if (isa<LoadInst>(GEPUserInst)) {
-                errs() << "Load: ";
-              } else if (isa<StoreInst>(GEPUserInst)) {
-                errs() << "Store: ";
+    // llvm/Analysis/LoopInfo.h
+    auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
+    int cnt = 0;
+    for (Loop *L : LI) {
+      errs() << "\nLoop " << cnt++ << ":\n";
+      for (auto bb = L->block_begin(), be = L->block_end(); bb != be; ++bb) {
+        for (BasicBlock::iterator instr = (*bb)->begin(), ee = (*bb)->end(); instr != ee; ++instr) {
+          // https://llvm.org/doxygen/classllvm_1_1Instruction.html
+          // ref: llvm/lib/Transforms/Scalar/LoopFlatten.cpp
+          if (auto *GEP = dyn_cast<GetElementPtrInst>(instr)) {
+            for (Value *GEPUser : instr->users()) {
+              auto *GEPUserInst = cast<Instruction>(GEPUser);
+              if (isa<LoadInst>(GEPUserInst) || isa<StoreInst>(GEPUserInst)) {
+                if (isa<LoadInst>(GEPUserInst)) {
+                  errs() << "Load: ";
+                } else if (isa<StoreInst>(GEPUserInst)) {
+                  errs() << "Store: ";
+                }
+                GEPUserInst->dump();
+                // print original array
+                errs() << "Original: ";
+                errs() << GEP->getPointerOperand()->getName() << "[";
+                for (auto i = GEP->idx_begin(), e = GEP->idx_end(); i != e;
+                     ++i) {
+                  if (i == GEP->idx_begin())
+                    continue;
+                  InstVisitor visitor;
+                  visitor.visit(i->get());
+                  std::string str = visitor.getIndexString();
+                  errs() << str;
+                }
+                errs() << "]\n";
               }
-              GEPUserInst->dump();
-              // print original array
-              errs() << "Original: ";
-              errs() << GEP->getPointerOperand()->getName() << "[";
-              for (auto i = GEP->idx_begin(), e = GEP->idx_end(); i != e; ++i) {
-                if (i == GEP->idx_begin())
-                  continue;
-                InstVisitor visitor;
-                visitor.visit(i->get());
-                std::string str = visitor.getIndexString();
-                errs() << str;
-              }
-              errs() << "]\n";
             }
           }
         }
       }
     }
-    // llvm/Analysis/LoopInfo.h
-    auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
-    auto &LAA = getAnalysis<LoopAccessLegacyAnalysis>();
-    for (Loop *L : LI) {
-      L->dumpVerbose();
-      errs() << "\n";
-      const LoopAccessInfo &LA = LAA.getInfo(L);
-      llvm::raw_ostream &output = llvm::outs();
-      LA.print(output);
-      ScalarEvolution &SE = getAnalysis<ScalarEvolutionWrapperPass>().getSE();
-      auto iv = L->getInductionVariable(SE);
-      assert(iv == NULL);
-      errs() << "# Loads:" << LA.getNumLoads() << "\n";
-      errs() << "# Stores:" << LA.getNumStores() << "\n";
-      // LAA.getInstructionsForAccess()
-    }
-    return false;
+    return true;
   }
 
   void getAnalysisUsage(AnalysisUsage &AU) const override {
     AU.addRequired<LoopInfoWrapperPass>();
     AU.addPreserved<LoopInfoWrapperPass>();
-    AU.addRequired<LoopAccessLegacyAnalysis>();
-    AU.addRequired<ScalarEvolutionWrapperPass>();
   }
 };
 } // namespace
--- Lesson7/test/test.c
+++ Lesson7/test/test.c
@@ -2,8 +2,22 @@
 
 int main() {
     int A[10], B[10], C[10];
-    for (int i = 0; i < 9; ++i) {
+    // simple
+    for (int i = 0; i < 10; ++i) {
         C[i] = A[i] + B[i];
     }
+    // stencil
+    for (int j = 1; j < 9; ++j) {
+        B[j] = A[j - 1] + A[j] + A[j + 1];
+    }
+    // complex indices
+    for (int k = 1; k < 4; ++k) {
+        C[k * 2] = A[k * 3 + 1] + B[k / 2];
+    }
+    // 2D: matmul
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            for (int k = 0; k < 3; ++k)
+                C[i * 3 + j] = A[i * 3 + k] * B[k * 3 + j];
     return 0;
 }
