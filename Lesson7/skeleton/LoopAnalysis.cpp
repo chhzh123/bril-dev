693f26696579ab295abefde0de6f661f9108ba88
chhzh123 <hc676@cornell.edu>
2022-03-15 21:30:37 -0400

    [L7] Add InstVisitor

--- Lesson7/skeleton/LoopAnalysis.cpp
+++ Lesson7/skeleton/LoopAnalysis.cpp
@@ -16,40 +16,71 @@
 using namespace llvm;
 
 namespace {
+
+class InstVisitor {
+public:
+  void visit(Value *inst) {
+    if (auto cast = dyn_cast<CastInst>(inst)) {
+      visit(cast->getOperand(0));
+    } else if (auto load = dyn_cast<LoadInst>(inst)) {
+      this->index += std::string(load->getOperand(0)->getName());
+    } else if (auto cst = dyn_cast<ConstantInt>(inst)) {
+      this->index += std::to_string(cst->getSExtValue());
+    } else if (auto bin = dyn_cast<BinaryOperator>(inst)) {
+      auto op0 = bin->getOperand(0);
+      auto op1 = bin->getOperand(1);
+      visit(op0);
+      if (bin->getOpcode() == Instruction::Add)
+        this->index += "+";
+      else if (bin->getOpcode() == Instruction::Sub)
+        this->index += "-";
+      else if (bin->getOpcode() == Instruction::Mul)
+        this->index += "*";
+      else if (bin->getOpcode() == Instruction::UDiv ||
+               bin->getOpcode() == Instruction::SDiv)
+        this->index += "/";
+      visit(op1);
+    }
+  }
+  std::string getIndexString() const { return index; }
+
+private:
+  std::string index = "";
+};
+
 struct LoopAnalysisPass : public FunctionPass {
   static char ID;
   LoopAnalysisPass() : FunctionPass(ID) {}
 
   virtual bool runOnFunction(Function &F) {
-    errs() << "I saw a function called " << F.getName() << "!\n";
     for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
       for (BasicBlock::iterator instr = bb->begin(), e = bb->end(); instr != e;
            ++instr) {
         // https://llvm.org/doxygen/classllvm_1_1Instruction.html
         // ref: llvm/lib/Transforms/Scalar/LoopFlatten.cpp
         if (auto *GEP = dyn_cast<GetElementPtrInst>(instr)) {
-          errs() << "Indices:" << GEP->getNumIndices() << "\n";
-          GEP->getPointerOperand()->dump();
-          errs() << GEP->getPointerOperand()->getName() << "\n";
-          for (auto i = GEP->idx_begin(), e = GEP->idx_end(); i != e; ++i) {
-            if (i == GEP->idx_begin())
-              continue;
-            // https://llvm.org/doxygen/classllvm_1_1Value.html
-            // Value* idx = i->get();
-            auto idx_inst = cast<Instruction>(i);
-            if (auto cast = dyn_cast<CastInst>(idx_inst)) {
-              errs() << "cast\n";
-              Value* op = cast->getOperand(0);
-              if (auto load = dyn_cast<LoadInst>(op)) {
-                errs() << load->getOperand(0)->getName() << "\n";
-              }
-            }
-            // idx_inst->dump();
-          }
           for (Value *GEPUser : instr->users()) {
             auto *GEPUserInst = cast<Instruction>(GEPUser);
-            if (isa<LoadInst>(GEPUserInst) || isa<StoreInst>(GEPUserInst))
+            if (isa<LoadInst>(GEPUserInst) || isa<StoreInst>(GEPUserInst)) {
+              if (isa<LoadInst>(GEPUserInst)) {
+                errs() << "Load: ";
+              } else if (isa<StoreInst>(GEPUserInst)) {
+                errs() << "Store: ";
+              }
               GEPUserInst->dump();
+              // print original array
+              errs() << "Original: ";
+              errs() << GEP->getPointerOperand()->getName() << "[";
+              for (auto i = GEP->idx_begin(), e = GEP->idx_end(); i != e; ++i) {
+                if (i == GEP->idx_begin())
+                  continue;
+                InstVisitor visitor;
+                visitor.visit(i->get());
+                std::string str = visitor.getIndexString();
+                errs() << str;
+              }
+              errs() << "]\n";
+            }
           }
         }
       }
