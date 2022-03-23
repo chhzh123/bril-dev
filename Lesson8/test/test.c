ae6199934b2ee4d79f88ed8b957ca91d23daad3e
chhzh123 <hc676@cornell.edu>
2022-03-23 12:13:17 -0400

    [L8] Update test

--- Lesson8/test/test.c
+++ Lesson8/test/test.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "sys/time.h"
 
-#define SIZE 2048
+#define SIZE 1024
 
 int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
@@ -9,10 +9,11 @@ int main() {
   // matmul
   struct timeval stop, start;
   gettimeofday(&start, NULL);
-  for (int i = 0; i < SIZE; ++i)
-    for (int j = 0; j < SIZE; ++j)
-      for (int k = 0; k < SIZE; ++k)
-        C[i][j] += A[i][k] * B[k][j];
+  for (int fake = 0; fake < 1; ++fake)
+    for (int i = 0; i < SIZE; ++i)
+      for (int j = 0; j < SIZE; ++j)
+        for (int k = 0; k < SIZE; ++k)
+          C[i][j] += A[i][k] * B[k][j];
   gettimeofday(&stop, NULL);
   printf("took %lu us\n", (stop.tv_sec - start.tv_sec) * 1000000 + stop.tv_usec - start.tv_usec); 
   printf("passed\n");
