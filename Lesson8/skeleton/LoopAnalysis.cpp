655888792ac8d0e4253cc1b2a480eb01a673e159
chhzh123 <hc676@cornell.edu>
2022-03-23 02:53:01 -0400

    [L8] Update loop reorder

--- Lesson8/skeleton/LoopAnalysis.cpp
+++ Lesson8/skeleton/LoopAnalysis.cpp
@@ -1,50 +1,19 @@
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils.h"
 #include "llvm/Transforms/Utils/LoopSimplify.h"
-#include "llvm/IR/CFG.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 using namespace llvm;
 
 namespace {
-
-class InstVisitor {
-public:
-  void visit(Value *inst) {
-    if (auto cast = dyn_cast<CastInst>(inst)) {
-      visit(cast->getOperand(0));
-    } else if (auto load = dyn_cast<LoadInst>(inst)) {
-      this->index += std::string(load->getOperand(0)->getName());
-    } else if (auto cst = dyn_cast<ConstantInt>(inst)) {
-      this->index += std::to_string(cst->getSExtValue());
-    } else if (auto bin = dyn_cast<BinaryOperator>(inst)) {
-      auto op0 = bin->getOperand(0);
-      auto op1 = bin->getOperand(1);
-      visit(op0);
-      if (bin->getOpcode() == Instruction::Add)
-        this->index += "+";
-      else if (bin->getOpcode() == Instruction::Sub)
-        this->index += "-";
-      else if (bin->getOpcode() == Instruction::Mul)
-        this->index += "*";
-      else if (bin->getOpcode() == Instruction::UDiv ||
-               bin->getOpcode() == Instruction::SDiv)
-        this->index += "/";
-      visit(op1);
-    }
-  }
-  std::string getIndexString() const { return index; }
-
-private:
-  std::string index = "";
-};
 
 /// Update LoopInfo, after interchanging. NewInner and NewOuter refer to the
 /// new inner and outer loop after interchanging: NewInner is the original
@@ -80,22 +49,28 @@ struct LoopAnalysisPass : public FunctionPass {
   //   }
   // }
 
+  // update branch operand
+  void updateBranchOperand(BranchInst *BI, BasicBlock *OldBB,
+                           BasicBlock *NewBB) {
+    for (Use &Op : BI->operands())
+      if (Op == OldBB) {
+        Op.set(NewBB);
+      }
+  }
+
   virtual bool runOnFunction(Function &F) {
     // llvm/Analysis/LoopInfo.h
     auto &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
     int cnt = 0;
-    Loop* OuterLoop = nullptr;
-    Loop* InnerLoop = nullptr;
+    Loop *OuterLoop = nullptr;
+    Loop *InnerLoop = nullptr;
     int OuterLoopIdx = 1;
     int InnerLoopIdx = 2;
     // llvm/Analysis/LoopInfo.h
     for (Loop *L : LI.getLoopsInPreorder()) {
-      llvm::errs() << L->getHeader()->getName() << "\n";
       if (OuterLoopIdx == cnt) {
-        llvm::errs() << "Outer Loop: " << L->getName() << "\n";
         OuterLoop = L;
       } else if (InnerLoopIdx == cnt) {
-        llvm::errs() << "Inner Loop: " << L->getName() << "\n";
         InnerLoop = L;
       }
       cnt++;
@@ -110,12 +85,16 @@ struct LoopAnalysisPass : public FunctionPass {
     BasicBlock *OuterLoopLatch = OuterLoop->getLoopLatch();
     BasicBlock *InnerLoopExit = InnerLoop->getExitBlock();
     BasicBlock *OuterLoopExit = OuterLoop->getExitBlock();
-    BasicBlock *InnerLoopBody = InnerLoopHeader->getNextNode();
-    InnerLoopLatch->dump();
-    InnerLoopExit->dump();
+    BasicBlock *InnerLoopSuccessor = InnerLoopHeader->getNextNode();
+    BasicBlock *OuterLoopPredecessor =
+        OuterLoopPreheader->getUniquePredecessor();
+    BasicBlock *InnerLoopLatchPredecessor =
+        InnerLoopLatch->getUniquePredecessor();
+    BasicBlock *OuterLoopExitSuccessor = OuterLoopExit->getNextNode();
+
     // move headers
     InnerLoopHeader->moveBefore(OuterLoopHeader);
-    OuterLoopHeader->moveBefore(InnerLoopBody);
+    OuterLoopHeader->moveBefore(InnerLoopSuccessor);
     // move preheaders
     InnerLoopPreheader->moveBefore(InnerLoopHeader);
     OuterLoopPreheader->moveBefore(OuterLoopHeader);
@@ -125,14 +104,34 @@ struct LoopAnalysisPass : public FunctionPass {
     InnerLoopLatch->moveBefore(InnerLoopExit);
     OuterLoopExit->moveBefore(InnerLoopLatch);
     OuterLoopLatch->moveBefore(OuterLoopExit);
-    F.dump();
 
-    // update branch op
-    // for (Use &Op : BI->operands())
-    //   if (Op == OldBB) {
-    //     Op.set(NewBB);
-    //     Changed = true;
-    //   }
+    // Update branch instructions
+    BranchInst *OuterLoopHeaderBI =
+        dyn_cast<BranchInst>(OuterLoopHeader->getTerminator());
+    BranchInst *InnerLoopHeaderBI =
+        dyn_cast<BranchInst>(InnerLoopHeader->getTerminator());
+    BranchInst *OuterLoopLatchBI =
+        dyn_cast<BranchInst>(OuterLoopLatch->getTerminator());
+    BranchInst *InnerLoopLatchBI =
+        dyn_cast<BranchInst>(InnerLoopLatch->getTerminator());
+    BranchInst *InnerLoopExitBI =
+        dyn_cast<BranchInst>(InnerLoopExit->getTerminator());
+    BranchInst *OuterLoopExitBI =
+        dyn_cast<BranchInst>(OuterLoopExit->getTerminator());
+    BranchInst *OuterLoopPredecessorBI =
+        dyn_cast<BranchInst>(OuterLoopPredecessor->getTerminator());
+    updateBranchOperand(OuterLoopPredecessorBI, OuterLoopHeader,
+                        InnerLoopPreheader);
+    updateBranchOperand(OuterLoopHeaderBI, InnerLoopPreheader,
+                        InnerLoopSuccessor);
+    updateBranchOperand(InnerLoopHeaderBI, InnerLoopSuccessor,
+                        OuterLoopPreheader);
+    updateBranchOperand(InnerLoopExitBI, OuterLoopLatch,
+                        OuterLoopExitSuccessor);
+    updateBranchOperand(OuterLoopExitBI, OuterLoopExitSuccessor,
+                        InnerLoopLatch);
+
+    F.dump();
     return true;
   }
 
