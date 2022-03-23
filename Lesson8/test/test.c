009a93f731a8cfd15ef9b6324a69907a91977b49
chhzh123 <hc676@cornell.edu>
2022-03-23 03:13:14 -0400

    [L8] Add timing

--- Lesson8/test/test.c
+++ Lesson8/test/test.c
@@ -1,13 +1,20 @@
 #include "stdio.h"
+#include "time.h"
+
+const int SIZE = 1024;
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
 int main() {
-    const int SIZE = 10;
-    int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
-    // matmul
-    for (int i = 0; i < SIZE; ++i)
-        for (int j = 0; j < SIZE; ++j)
-            for (int k = 0; k < SIZE; ++k)
-                C[i][j] += A[i][k] * B[k][j];
-    printf("passed\n");
-    return 0;
+  // matmul
+  clock_t start, stop;
+  start = clock();
+  for (int i = 0; i < SIZE; ++i)
+    for (int j = 0; j < SIZE; ++j)
+      for (int k = 0; k < SIZE; ++k)
+        C[i][j] += A[i][k] * B[k][j];
+  stop = clock();
+  printf("passed\n");
+  printf("%6.8f\n", start);
+  printf("%6.8f\n", stop);
+  return 0;
 }
